f3dc63094e655dee637534192489b8a95a184893
Devyn Gish <devyngish@gmail.com>
2019-08-21 18:31:08 -0600

    first push

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
 root "departments#index"
+
+
+resources :departments do
+  resources :items
+
+get "/departments", to: "departments#index", as: 'all_the_departments'
+get "/department/:id", to: "department#show", as: 'the_one_department'
+delete "/department/:id", to: "department#destroy", as: 'destroy_the_department'
+
+
+end
 end
